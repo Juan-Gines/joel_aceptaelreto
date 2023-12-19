abe4c30915a2d270bb9a21cd22b4e3b89819493d
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 18:52:08 +0100

    contar paraules no funciona bien

---
+++ src/Joel/Paraules.java
@@ -0,0 +1,18 @@
+package Joel;
+
+import java.util.Scanner;
+
+public class Paraules {
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        int casos = in.nextInt();
+        in.nextLine();
+        for (int i = 0; i < casos; i++) {
+            String frase = in.nextLine();
+            frase = frase.trim();
+            String[] palabrasFrase = frase.split("\\s+");
+            System.out.println(palabrasFrase.length);
+        }
+       
+    }
+}
