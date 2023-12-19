9ebf62555c2edcaf674c103246e69fd5629052d4
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 19:19:06 +0100

    ControlNoms ok

---
+++ src/Joel/ControlNoms.java
@@ -0,0 +1,18 @@
+package Joel;
+
+import java.util.Scanner;
+
+public class ControlNoms {
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        String ranq = in.nextLine();
+        String resultado = "NO";
+        for (int i = 0; i < ranq.length(); i++) {
+            if (ranq.charAt(0)!=ranq.charAt(i)){
+                resultado = "SI";
+                break;
+            }
+        }
+        System.out.println(resultado);
+    }
+}
