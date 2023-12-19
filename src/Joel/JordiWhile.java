002606f94e9f44eb71ddc1399c88ab5462b9ad75
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 20:03:22 +0100

    JordiWhile ok

---
+++ src/Joel/JordiWhile.java
@@ -0,0 +1,17 @@
+package Joel;
+
+import java.util.Scanner;
+
+public class JordiWhile {
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        int strikes =0;
+        int contador = 0;
+        do {
+            int num = in.nextInt();
+            if (num == -1) strikes++;
+            else contador++;
+        } while (strikes < 3);
+        System.out.println(contador);
+    }
+}
