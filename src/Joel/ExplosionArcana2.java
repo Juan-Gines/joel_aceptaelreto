99636322dd10c3872f092543f5e3bb4f225bdc0a
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 19:31:25 +0100

    ExplosionArcana2 ok

---
+++ src/Joel/ExplosionArcana2.java
@@ -0,0 +1,18 @@
+package Joel;
+
+import java.util.Scanner;
+
+public class ExplosionArcana2 {
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        int dany = in.nextInt();
+        int hp = in.nextInt();
+        int contador = 0;        
+        do {
+            contador++;
+            hp -= dany*contador;
+        } while (hp > 0);
+        
+        System.out.println(contador);
+    }
+}
