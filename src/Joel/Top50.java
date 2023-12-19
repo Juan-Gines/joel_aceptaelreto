560f755971516f1dee5750ca185dd552b4bb0b60
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 19:11:20 +0100

    top50 ok

--- src/Joel/Top50.java
+++ src/Joel/Top50.java
@@ -1,5 +1,15 @@
 package Joel;
 
+import java.util.Scanner;
+
 public class Top50 {
-    
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        int ranq = in.nextInt();
+        int torneos = in.nextInt();        
+        for (int i = 0; i < torneos; i++) {
+            ranq /= 2;
+        }
+        System.out.println(ranq);
+    }
 }
