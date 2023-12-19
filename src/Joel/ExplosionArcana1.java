f93bc7ff2ef94d2979ce3b57cd8f1cfbb2dc626b
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 19:23:36 +0100

    Explosion arcana 1 ok

---
+++ src/Joel/ExplosionArcana1.java
@@ -0,0 +1,16 @@
+package Joel;
+
+import java.util.Scanner;
+
+public class ExplosionArcana1 {
+    public static void main(String[] args) {
+        Scanner in = new java.util.Scanner(System.in);
+        int dany = in.nextInt();
+        int explosions = in.nextInt();
+        int total = 0;        
+        for (int i = 0; i < explosions; i++) {
+            total += dany * (i +1);
+        }
+        System.out.println(total);
+    }
+}
