b0273131f6d2e4d21750f9c391885ff534b31a01
Juan-Gines <juangalenta@hotmail.com>
2023-12-19 16:29:01 +0100

    contar caracteres da mal

--- src/Joel/ContarCaracters.java
+++ src/Joel/ContarCaracters.java
@@ -1,9 +1,24 @@
 package Joel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class ContarCaracters {
     public static void main(String[] args) {
         Scanner in = new java.util.Scanner(System.in);
-        int casos = in.nextInt();
-        in.nextLine();
+        List<Integer> letras = new ArrayList<Integer>();
+        String frase = "";
+        while (!frase.equals("FI")) {
+            frase = in.nextLine();
+            int contador = 0;
+            for (int i = 0; i < frase.length(); i++) {
+                if (Character.isLetter(frase.charAt(i))) {
+                    contador++;
+                }
+            } 
+            if(!frase.equals("FI"))letras.add(contador);          
+        }
+        for (int i = 0; i < letras.size(); i++) {
+            System.out.print(letras.get(i) + " ");
+        }        
     }
 }
